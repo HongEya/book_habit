ad0625bd8d319dffdbe3ba34f262f2f7315b3cc3
hongEya <qaz7q@naver.com>
2021-12-19 19:54:54 +0900

    첫 프로젝트 업로드

---
+++ src/main/java/com/greenart/book_habit/BookHabitApplication.java
@@ -0,0 +1,13 @@
+package com.greenart.book_habit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookHabitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenart/book_habit/BookHabitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenart.book_habit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHabitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
